3a134d689e3e70d9578571fa1e4648b77dc75408
Lawrence Collins <67695762+lawrencecollins@users.noreply.github.com>
2024-03-12 08:36:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="deconvolawrence",
 
-    version="0.2.9.1",
+    version="0.2.9.2",
 
     author="Lawrence Collins",
     author_email="cm19ljc@leeds.ac.uk",
